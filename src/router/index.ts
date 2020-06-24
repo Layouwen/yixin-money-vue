1ad1d7c78eace5376f7bfe4cb95690d6370817f6
layouwen <layouwen@gmail.com>
2020-06-24 11:17:31 +0800

    add router 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, {RouteConfig} from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -22,20 +23,10 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistics
+  }, {
+    path: '*',
+    component: NotFound
   }
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    你访问的路径不存在，请重试~
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
