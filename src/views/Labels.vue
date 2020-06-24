bb3c59b7b4bb89afd339cdbfab6b9c2412ea6daf
layouwen <layouwen@gmail.com>
2020-06-24 09:58:50 +0800

    add router

---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是标签</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Labels'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是记账</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Money'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
