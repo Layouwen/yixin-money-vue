f5fb3b99be014ed544cb36451f00a327dd12f818
layouwen <layouwen@gmail.com>
2020-06-25 10:20:07 +0800

    引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <router-link to="/money">记账</router-link>
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/money">
+      记账
+    </router-link>
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#add"></use>
+      </svg>
+      标签
+    </router-link>
     <router-link to="/statistics">统计</router-link>
   </div>
 </template>
