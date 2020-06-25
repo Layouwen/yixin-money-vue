4b99528562be874a6339cd5b883e6bf12382070f
layouwen <layouwen@gmail.com>
2020-06-25 10:36:44 +0800

    自动引svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <router-link to="/money">
-      记账
-    </router-link>
     <router-link to="/labels">
       <svg>
-        <use xlink:href="#add"></use>
+        <use xlink:href="#label"></use>
       </svg>
       标签
     </router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#add"></use>
+      </svg>
+      记账
+    </router-link>
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"></use>
+      </svg>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
-  import x from '@/assets/icons/add.svg';
+  // 自动引入所有svg
+  const importAll = (requireContent: __WebpackModuleApi.RequireContext) => requireContent.keys().forEach(requireContent);
+  try {
+    importAll(require.context('../assets/icons',true,/\.svg$/));
+  }catch(error){
+    console.log(error)
+  }
 
-  console.log(x);
   export default {
     name: 'Nav.vue'
   };
