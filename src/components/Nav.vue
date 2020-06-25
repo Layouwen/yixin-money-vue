e8b2b52b51cbd55870a87e48ea56a65c659638c5
layouwen <layouwen@gmail.com>
2020-06-25 11:11:35 +0800

    添加路由激活

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="add"/>
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -38,6 +38,9 @@
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected {
+      color: blue;
     }
   }
 </style>
