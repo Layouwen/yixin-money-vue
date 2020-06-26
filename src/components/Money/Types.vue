045908a7b2ea642159f37d03a372f0158dfb857c
layouwen <layouwen@gmail.com>
2020-06-26 18:03:12 +0800

    实现了Type栏的切换

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,16 +1,33 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'"
+          @click="selectedType('-')">支出
+      </li>
+      <li :class="type === '+' && 'selected'"
+          @click="selectedType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
   export default {
-    name: 'Types'
-  };
+    name: 'Types',
+    data() {
+      return {
+        type: '-' // 减号表示支出，加号表示收入
+      }
+    },
+    methods: {
+      selectedType(type) {
+        if (type !== '-' && type !== '+') {
+          throw new Error('type is unknow')
+        }
+        this.type = type
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
