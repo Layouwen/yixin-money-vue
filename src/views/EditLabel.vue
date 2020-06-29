0699b9bca5b1731a934f0daa7d1cac338e3b046d
layouwen <layouwen@gmail.com>
2020-06-29 12:50:42 +0800

    将tag的string改为{id, name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    编辑页面
+  </Layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  @Component
+  export default class EditLabel extends Vue {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
