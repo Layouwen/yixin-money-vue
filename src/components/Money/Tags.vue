83ed3fc3e7f6d6c0d7ba84a7f18ed355d784e3e9
Layouwen <Layouwen@gmail.com>
2020-07-07 11:14:21 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,14 +19,16 @@
   @Component({
     computed: {
       tagList() {
-        // TODO
-        // return this.$store.state.tagList;
-        return [];
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
+
+    created() {
+      this.$store.commit('fetchTags');
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -41,8 +43,7 @@
     create() {
       const name = window.prompt('请输入内容');
       if (!name) {return window.alert('标签名不能为空');}
-      // TODO
-      // store.createTag(name);
+      this.$store.commit('createTag', name);
     }
   }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, {mapMutations} from 'vuex';
 import clone from '@/lib/clone';
+import createId from '@/lib/createId';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     recordList: [] as RecordItem[],
+    tagList: [] as Tag[],
   },
   mutations: {
     fetchRecords(state) {
@@ -20,7 +22,25 @@ const store = new Vuex.Store({
       record2.createAt = new Date();
       state.recordList.push(record2);
       store.commit('saveRecords');
-    }
+    },
+    fetchTags(state) {
+      return state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+    },
+    createTag(state, name: string) {
+      const names = state.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        window.alert('标签名重复了');
+        return 'duplicated';
+      }
+      const id = createId().toString();
+      state.tagList.push({id: id, name: name});
+      store.commit('saveTags');
+      window.alert('添加成功');
+      return 'success';
+    },
+    saveTags(state) {
+      return window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
+    },
   }
 });
 
