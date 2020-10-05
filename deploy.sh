ce8aa3643490f42a79d90c324c41b9ad8d8fa9d1
Layouwen <Layouwen@gmail.com>
2020-10-06 01:35:29 +0800

    add github url

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@gitee.com:layouwen/yixin-money-vue-website.git master
+#git push -f git@gitee.com:layouwen/yixin-money-vue-website.git master
+git push -f git@github.com:layouwen/yixin-money-vue-website.git master
 
 cd -
